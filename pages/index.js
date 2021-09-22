9b7ec1f0e45e0ed6d175d4f5de0aa47bbbeec2cd
h4mxa <duke80800@gmail.com>
2021-09-22 16:17:30 -0700

    rendered movie list data

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 
 // --------- Components -------------
@@ -8,59 +8,66 @@ import Carousel from '../components/carousel/carousel.component';
 import MovieList from '../components/movie-list/movie-list.component';
 import Footer from '../components/footer/footer';
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link
-        rel="stylesheet"
-        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-        crossOrigin="anonymous"
-      />
-      <script
-        src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossOrigin="anonymous"
-      ></script>
-      <script
-        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossOrigin="anonymous"
-      ></script>
-      <script
-        src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossOrigin="anonymous"
-      ></script>
-    </Head>
+// ---------- data ------------------
+import MovieData from './data/movie-data';
 
-    <Navbar />
-    <div className="home-page">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-3">
-            <SideMenu />
-          </div>
+const Movie_Data = MovieData;
 
-          <div className="col-lg-9">
-            <Carousel />
-            <div className="row">
-              <MovieList />
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossOrigin="anonymous"
+        />
+        <script
+          src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+          integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+          integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+          integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+
+      <Navbar />
+      <div className="home-page">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-3">
+              <SideMenu />
+            </div>
+
+            <div className="col-lg-9">
+              <Carousel />
+              <div className="row">
+                <MovieList movies={Movie_Data} />
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <Footer />
+      <style jsx>
+        {`
+          .home-page {
+            padding-top: 80px;
+          }
+        `}
+      </style>
     </div>
-    <Footer />
-    <style jsx>
-      {`
-        .home-page {
-          padding-top: 80px;
-        }
-      `}
-    </style>
-  </div>
-);
+  );
+};
 
 export default Home;
